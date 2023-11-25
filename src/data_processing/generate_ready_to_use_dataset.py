756c8620940fbda2971a0ba5bb89bfb5413f895f
Valentin De Matos <vdmatos@gladia.io>
2023-11-25 20:43:26 +0000

    refactor(generate_ready_to_use_dataset): keep only input_ids and labels
    Signed-off-by: Valentin De Matos <vdmatos@gladia.io>

--- src/data_processing/generate_ready_to_use_dataset.py
+++ src/data_processing/generate_ready_to_use_dataset.py
@@ -36,6 +36,8 @@ def main(
     path_to_output_dataset: str,
 ) -> None:
 
+    COLUMNS_TO_KEEP = ["input_ids", "labels"]
+
     tokenizer = get_tokenizer()
 
     test_set = Dataset.from_csv(path_to_test_set).map(
@@ -45,6 +47,7 @@ def main(
         },
         num_proc=os.cpu_count(),
     )
+    test_set = test_set.remove_columns([c for c in test_set.column_names if c not in COLUMNS_TO_KEEP])
 
     train_set = Dataset.from_csv(path_to_train_set).map(
         generate_and_tokenize_prompt,
@@ -53,6 +56,7 @@ def main(
         },
         num_proc=os.cpu_count(),
     )
+    train_set = train_set.remove_columns([c for c in train_set.column_names if c not in COLUMNS_TO_KEEP])
 
     DatasetDict({
         "test": test_set,
