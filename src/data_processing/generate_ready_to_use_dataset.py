8913c0b47429f6ecc2aa441d1ec5b3f299b209be
Valentin De Matos <vdmatos@gladia.io>
2023-11-25 20:57:28 +0000

    refactor(generate_ready_to_use_dataset): force clear cache
    Signed-off-by: Valentin De Matos <vdmatos@gladia.io>

--- src/data_processing/generate_ready_to_use_dataset.py
+++ src/data_processing/generate_ready_to_use_dataset.py
@@ -40,23 +40,25 @@ def main(
 
     tokenizer = get_tokenizer()
 
-    test_set = Dataset.from_csv(path_to_test_set).map(
+    test_set = Dataset.from_csv(path_to_test_set)
+    test_set.cleanup_cache_files()
+    test_set = test_set.map(
         generate_and_tokenize_prompt,
         fn_kwargs={
             "tokenizer": tokenizer,
         },
         num_proc=os.cpu_count(),
-    )
-    test_set = test_set.remove_columns([c for c in test_set.column_names if c not in COLUMNS_TO_KEEP])
+    ).remove_columns([c for c in test_set.column_names if c not in COLUMNS_TO_KEEP])
 
-    train_set = Dataset.from_csv(path_to_train_set).map(
+    train_set = Dataset.from_csv(path_to_train_set)
+    train_set.cleanup_cache_files()
+    train_set = train_set.map(
         generate_and_tokenize_prompt,
         fn_kwargs={
             "tokenizer": tokenizer,
         },
         num_proc=os.cpu_count(),
-    )
-    train_set = train_set.remove_columns([c for c in train_set.column_names if c not in COLUMNS_TO_KEEP])
+    ).remove_columns([c for c in train_set.column_names if c not in COLUMNS_TO_KEEP])
 
 
     DatasetDict({
